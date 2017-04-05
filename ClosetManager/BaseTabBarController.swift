5d195a90d7990f52d25b6cb27721f52b6b583341
Aniruddha Borah <aniruddhaborah@gmail.com>
2017-04-05 02:30:01 -0400

    added new controllers and base navigation view controller

--- ClosetManager/BaseTabBarController.swift
+++ ClosetManager/BaseTabBarController.swift
@@ -12,6 +12,26 @@ class BaseTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpControllers()
+    }
+    
+    func setUpControllers() {
+        let explorerVC = ExplorerViewController(nibName: "ExplorerViewController", bundle: nil)
+        let createItemVC = CreateItemViewController(nibName: "CreateItemViewController", bundle: nil)
+        
+        let navExplorer = UINavigationController(rootViewController: explorerVC)
+        let navCreate = UINavigationController(rootViewController: createItemVC)
+        
+        navExplorer.tabBarItem = UITabBarItem(
+            title: "",
+            image: #imageLiteral(resourceName: "search"),
+            tag: 1)
+        navCreate.tabBarItem = UITabBarItem(
+            title: "",
+            image: #imageLiteral(resourceName: "add"),
+            tag: 1)
+        
+        self.viewControllers = [navExplorer, navCreate]
     }
 
     override func didReceiveMemoryWarning() {
